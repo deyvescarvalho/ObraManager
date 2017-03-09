fa40764ca7dba0fd66fe71565ece56a444150ace
Deyves Carvalho <deyvescarvalho@gmail.com>
2017-03-09 18:13:39 -0300

    novas rotas adficionadas

--- routes/web.php
+++ routes/web.php
@@ -41,13 +41,25 @@
 
 Route::group(['prefix'=>'aula'], function(){
   Route::get('/home', ['as' => 'aula.index', 'uses' => 'AulaController@index']);
+  Route::get('/clientes', ['as' => 'aula.listagem_clientes', 'uses' => 'AulaController@clientes']);
   Route::get('/cadastrar', ['as' => 'aula.create', 'uses' => 'AulaController@create']);
+  Route::put('/cliente/edit/{id}', ['as' => 'aula.update', 'uses' => 'AulaController@update']);
+  Route::get('/cliente/edit/{id}', ['as' => 'aula.edit', 'uses' => 'AulaController@edit']);
+  Route::get('/cliente/delete/{id}', ['as' => 'aula.destroy', 'uses' => 'AulaController@destroy']);
   Route::post('/cadastrar', ['as' => 'aula.store', 'uses' => 'AulaController@store']);
 });
 
-Route::get('/', ['middleware'=>'auth', 'PostsController@index']);
-Route::get('/test', 'TestController@index');
-Route::get('/notas', 'TestController@notas');
+// Route::get('/', ['middleware'=>'auth', 'PostsController@index']);
+// Route::get('/test', 'TestController@index');
+// Route::get('/notas', 'TestController@notas');
+
+Route::get('/', 'ClienteController@index');
+Route::get('/cliente', ['as'=>'cliente.listagem', 'uses'=>'ClienteController@clientes']);
+Route::get('/cliente/novo', ['as'=>'cliente.novo', 'uses'=>'ClienteController@create']);
+Route::post('/cliente/novo', ['as'=>'cliente.store', 'uses'=>'ClienteController@store']);
+Route::get('/cliente/edit/{id}', ['as'=>'cliente.edit', 'uses'=>'ClienteController@edit']);
+Route::put('/cliente/edit/{id}', ['as'=>'cliente.update', 'uses'=>'ClienteController@update']);
+Route::get('/cliente/delete/{id}', ['as'=>'cliente.destroy', 'uses'=>'ClienteController@destroy']);
 
 Route::group(['prefix'=>'blog', 'middleware'=>'auth'], function()
 {
@@ -59,5 +71,22 @@ Route::group(['prefix'=>'blog', 'middleware'=>'auth'], function()
 });
 
 Auth::routes();
+
+Route::get('/api/cliente', 'ClienteController@index');
+Route::post('/api/cliente', 'ClienteController@store');
+Route::get('/api/cliente/{id}', 'ClienteController@show');
+Route::delete('/api/cliente/{id}', 'ClienteController@destroy');
+
+Route::get('/funcionario', ['as'=>'funcionario.listagem', 'uses'=>'FuncionarioController@index']);
+
+Route::get('/funcionario/edit/{id}', ['as'=>'funcionario.edit', 'uses'=>'FuncionarioController@edit']);
+
+Route::put('/funcionario/edit/{id}', ['as'=>'funcionario.update', 'uses'=>'FuncionarioController@update']);
+
+Route::get('/funcionario/delete/{id}', ['as'=>'funcionario.destroy', 'uses'=>'FuncionarioController@destroy']);
+
+Route::get('/funcionario/novo', ['as'=>'funcionario.create', 'uses'=>'FuncionarioController@create']);
+
+Route::post('/funcionario/novo', ['as'=>'funcionario.store', 'uses'=>'FuncionarioController@store']);
 
 Route::get('/home', 'HomeController@index');
