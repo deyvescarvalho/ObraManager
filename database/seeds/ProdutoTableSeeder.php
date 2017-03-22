05178a2defe974f88a9a9c53c93a727ebae724b4
Deyves Carvalho <deyvescarvalho@gmail.com>
2017-03-22 17:06:10 -0300

    alteracoes seed e pasta publica

---
+++ database/seeds/ProdutoTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProdutoTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\Produto::truncate();
+        factory('App\Produto',20)->create();
+    }
+}
