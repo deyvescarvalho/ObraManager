d6c8891508471c35a69a6add2b0a3dbc07849f2d
Deyves Carvalho <deyvescarvalho@gmail.com>
2017-03-22 17:04:18 -0300

    alterações no controller de projeto

--- app/Http/Controllers/ProjetoController.php
+++ app/Http/Controllers/ProjetoController.php
@@ -4,74 +4,87 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Projeto;
+use App\Cliente;
 class ProjetoController extends Controller
 {
 
-    private $projeto;
+  private $projeto;
 
-    public function __construct(Projeto $projeto)
-    {
-      $this->projeto = $projeto;
-    }
+  public function __construct(Projeto $projeto)
+  {
+    $this->projeto = $projeto;
+  }
 
 
-    public function index()
-    {
-      $projetos = $this->projeto->paginate(10);
+  public function index()
+  {
+    $projetos = $this->projeto->paginate(5);
 
-      return view('projeto.listagem', compact('projetos'));
-    }
 
-    public function create()
-    {
-      return view('projeto.cadastro');
-    }
+    return view('projeto.listagem', compact('projetos'));
 
-    public function store(Request $request)
-    {
+  }
 
-      $this->validate($request, [
-        'endereco' => 'required',
-        'cliente' => 'required',
-        'cidade' => 'required',
-        'precoProjeto' => 'required'
-      ]);
+  public function view($id)
+  {
+    $total =0;
+    $somaQtd =0;
+    $somaCampo =0;
+    $projeto = Projeto::find($id);
+     return view('projeto.view', compact(['projeto', 'total', 'somaCampo', 'somaQtd']));
+  }
 
-      $this->projeto->create($request->all());
+  public function create()
+  {
+    $clientes = Cliente::all();
+    return view('projeto.cadastro', compact('clientes'));
+  }
 
-      return redirect()->
-        route('projeto.cadastro')->
-        with('status', 'Projeto criado com sucesso!');
-    }
+  public function store(Request $request)
+  {
 
-    public function edit($id)
-    {
-      $projeto = $this->projeto->find($id);
+    $this->validate($request, [
+      'endereco' => 'required',
+      // 'cliente' => 'required',
+      'cidade' => 'required',
+      'valorobra' => 'required'
+    ]);
 
-      return view('projeto.edit', compact('projeto'));
-    }
+    $this->projeto->create($request->all());
 
-    public function update($id, Request $request)
-    {
+    return redirect()->route('projeto.create')->with('status', 'Projeto criado com sucesso!');
+  }
 
-      $this->validate($request, [
-        'endereco' => 'required',
-        'cliente' => 'required',
-        'cidade' => 'required',
-        'precoProjeto' => 'required'
-      ]);
+  public function edit($id)
+  {
 
-      $this->projeto->find($id)->update($request->all());
+    $projeto = $this->projeto->find($id);
+    $clientes = Cliente::all();
 
-      return redirect()->route('projeto.listagem')->with('status', 'Projeto alterado com sucesso!');
+    return view('projeto.edit', compact(['projeto', 'clientes']));
+  }
 
-    }
+  public function update($id, Request $request)
+  {
 
-    public function destroy($id)
-    {
-      $this->projeto->find($id)->delete();
+    $this->validate($request, [
+      'endereco' => 'required',
+      // 'cliente' => 'required',
+      'cidade' => 'required',
+      'valorobra' => 'required'
+    ]);
 
-      return redirect()->route('projeto.listagem')->with('status', 'Projeto excluído com sucesso');
-    }
+    $this->projeto->find($id)->update($request->all());
+
+    return redirect()->route('projeto.listagem')->with('status', 'Projeto alterado com sucesso!');
+
+  }
+
+  public function destroy($id)
+  {
+    $this->projeto->find($id)->delete();
+
+    return redirect()->route('projeto.listagem')->with('status', 'Projeto excluído com sucesso');
+  }
 
 }
