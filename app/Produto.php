6add72956128ac99a1db26789f765504df854aaa
Deyves Carvalho <deyvescarvalho@gmail.com>
2017-03-22 17:04:33 -0300

    alterações gerais em produto projeto e user model

--- app/Produto.php
+++ app/Produto.php
@@ -8,6 +8,21 @@ class Produto extends Model
 {
     protected $fillable = [
       'descricao',
-      'categoria'
+      'categoria_id'
     ];
+
+    public function categoria()
+    {
+      return $this->belongsTo(Categoria::class);
+    }
+
+    public function projetos()
+    {
+      return $this->belongsToMany(Projeto::class, 'lancamento_projetos');
+    }
+    //
+    // public function fornecedores()
+    // {
+    //   return $this->belongsToMany(Fornecedor::class, 'lancamento_projetos')->withPivot(['projeto_id','qtdItem']);
+    // }
 }
--- app/Projeto.php
+++ app/Projeto.php
@@ -3,11 +3,38 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+// use Illuminate\Database\Eloquent\Relations\belongsTo;
+use App\LancamentoProjeto;
+use App\Cliente;
+use App\Funcionario;
 class Projeto extends Model
 {
     protected $fillable = [
+      'cliente_id',
       'endereco',
-      'precoProjeto'
+      'cidade',
+      'valorobra'
     ];
+
+    public function cliente()
+    {
+      return $this->belongsTo(Cliente::class);
+    }
+
+    public function funcionarios()
+    {
+      return $this->belongsToMany(Funcionario::class, 'lancamento_projetos_funcionarios')->withPivot(['dataLancamento', 'valorSalario']);
+    }
+
+    public function produtos()
+    {
+      return $this->belongsToMany(Produto::class, 'lancamento_projetos')->withPivot(['fornecedor_id','dataLancamento','valorItem','qtdItem']);
+    }
+
+    // public function fornecedores()
+    // {
+    //   return $this->belongsToMany(Fornecedor::class, 'lancamento_projetos');
+    // }
+
+
 }
