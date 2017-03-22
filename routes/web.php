c78daeeadb4945a46ad2cd8dc2b96c14fd8bb908
Deyves Carvalho <deyvescarvalho@gmail.com>
2017-03-22 17:03:19 -0300

    alteração no arquivos de rotas

--- routes/web.php
+++ routes/web.php
@@ -29,15 +29,16 @@
 //
 // });
 
-// Route::get('/auth/logout', function(){
-//   Auth::logout();
-// });
+Route::get('/auth/logout', ['as' =>'auth.logout', function(){
+  Auth::logout();
+  return redirect()->route('home');
+}]);
 
 // Route::get('/auth/login', function(){
 //   Auth::logout();
 // });
 
-// Route::get('/auth/login', 'Auth\AuthController@getLogin');
+Route::get('/auth/login', 'Auth\AuthController@getLogin');
 
 Route::group(['prefix'=>'aula'], function(){
   Route::get('/home', ['as' => 'aula.index', 'uses' => 'AulaController@index']);
@@ -61,6 +62,109 @@ Route::get('/cliente/edit/{id}', ['as'=>'cliente.edit', 'uses'=>'ClienteControll
 Route::put('/cliente/edit/{id}', ['as'=>'cliente.update', 'uses'=>'ClienteController@update']);
 Route::get('/cliente/delete/{id}', ['as'=>'cliente.destroy', 'uses'=>'ClienteController@destroy']);
 
+Route::post('/lancamento', ['as'=>'lancamento.store', 'uses'=>'LancamentoProjetoController@store']);
+Route::post('/lancamento/funcionario', ['as'=>'lancamento.lancaFuncionario', 'uses'=>'LancamentoProjetoController@lancaFuncionario']);
+Route::get('/lancamento/{id}', ['as'=>'lancamento.create', 'uses'=>'LancamentoProjetoController@create']);
+
+
+Route::group(['as'=>'projeto.', 'prefix'=>'projeto'], function ()
+{
+
+  Route::get('', ['as'=>'listagem', 'uses'=>'ProjetoController@index']);
+
+  Route::get('view/{id}', ['as'=>'view', 'uses'=>'ProjetoController@view']);
+
+  Route::get('novo', ['as'=>'create', 'uses'=>'ProjetoController@create']);
+
+  Route::post('novo', ['as'=>'store', 'uses'=>'ProjetoController@store']);
+
+
+  Route::get('edit/{id}', ['as' => 'edit', 'uses'=>'ProjetoController@edit']);
+
+  Route::put('edit/{id}', ['as'=>'update', 'uses'=>'ProjetoController@update']);
+
+  Route::get('destroy/{id}', ['as'=>'destroy', 'uses'=>'ProjetoController@destroy']);
+
+});
+
+Route::group(['as'=>'cargo.', 'prefix'=>'cargo'], function ()
+{
+
+  Route::get('', ['as'=>'listagem', 'uses'=>'CargoController@index']);
+
+  Route::get('novo', ['as'=>'create', 'uses'=>'CargoController@create']);
+
+  Route::post('novo', ['as'=>'store', 'uses'=>'CargoController@store']);
+
+
+  Route::get('edit/{id}', ['as' => 'edit', 'uses'=>'CargoController@edit']);
+
+  Route::put('edit/{id}', ['as'=>'update', 'uses'=>'CargoController@update']);
+
+  Route::get('destroy/{id}', ['as'=>'destroy', 'uses'=>'CargoController@destroy']);
+
+});
+
+
+
+Route::group(['as'=>'fornecedor.', 'prefix'=>'fornecedor', 'middleware' => 'auth'], function ()
+{
+
+  Route::get('', ['as'=>'listagem', 'uses'=>'FornecedorController@index']);
+
+  Route::get('novo', ['as'=>'create', 'uses'=>'FornecedorController@create']);
+
+  Route::post('novo', ['as'=>'store', 'uses'=>'FornecedorController@store']);
+
+
+  Route::get('edit/{id}', ['as' => 'edit', 'uses'=>'FornecedorController@edit']);
+
+  Route::put('edit/{id}', ['as'=>'update', 'uses'=>'FornecedorController@update']);
+
+  Route::get('destroy/{id}', ['as'=>'destroy', 'uses'=>'FornecedorController@destroy']);
+
+});
+
+Route::group(['as'=>'produto.', 'prefix'=>'produto'], function ()
+{
+
+  Route::get('', ['as'=>'listagem', 'uses'=>'ProdutoController@index']);
+
+  Route::get('novo', ['as'=>'create', 'uses'=>'ProdutoController@create']);
+
+  Route::post('novo', ['as'=>'store', 'uses'=>'ProdutoController@store']);
+
+
+  Route::get('edit/{id}', ['as' => 'edit', 'uses'=>'ProdutoController@edit']);
+
+  Route::put('edit/{id}', ['as'=>'update', 'uses'=>'ProdutoController@update']);
+
+  Route::get('destroy/{id}', ['as'=>'destroy', 'uses'=>'ProdutoController@destroy']);
+
+});
+
+
+Route::group(['as'=>'categoria.', 'prefix'=>'categoria'], function ()
+{
+
+  Route::get('', ['as'=>'listagem', 'uses'=>'CategoriaController@index']);
+
+  Route::get('novo', ['as'=>'create', 'uses'=>'CategoriaController@create']);
+
+  Route::post('novo', ['as'=>'store', 'uses'=>'CategoriaController@store']);
+
+
+  Route::get('edit/{id}', ['as' => 'edit', 'uses'=>'CategoriaController@edit']);
+
+  Route::put('edit/{id}', ['as'=>'update', 'uses'=>'CategoriaController@update']);
+
+  Route::get('destroy/{id}', ['as'=>'destroy', 'uses'=>'CategoriaController@destroy']);
+
+});
+
+
+
+
 Route::group(['prefix'=>'blog', 'middleware'=>'auth'], function()
 {
   Route::post('/', ['as' => 'blog.store', 'uses' =>'PostsController@store']);
@@ -83,6 +187,8 @@ Route::get('/funcionario/edit/{id}', ['as'=>'funcionario.edit', 'uses'=>'Funcion
 
 Route::put('/funcionario/edit/{id}', ['as'=>'funcionario.update', 'uses'=>'FuncionarioController@update']);
 
+Route::get('/funcionario/view/{id}', ['as'=>'funcionario.detalhe', 'uses'=>'FuncionarioController@view']);
+
 Route::get('/funcionario/delete/{id}', ['as'=>'funcionario.destroy', 'uses'=>'FuncionarioController@destroy']);
 
 Route::get('/funcionario/novo', ['as'=>'funcionario.create', 'uses'=>'FuncionarioController@create']);
@@ -90,20 +196,9 @@ Route::get('/funcionario/novo', ['as'=>'funcionario.create', 'uses'=>'Funcionari
 Route::post('/funcionario/novo', ['as'=>'funcionario.store', 'uses'=>'FuncionarioController@store']);
 
 
-Route::group(['prefix'=>'projeto'], function ()
-{
-
-  Route::get('', ['as'=>'projeto.listagem', 'uses'=>'ProjetoController@index']);
-
-  Route::get('novo', ['as'=>'projeto.create', 'uses'=>'ProjetoController@create']);
-
-  Route::post('novo', ['as'=>'projeto.store', 'uses'=>'ProjetoController@store']);
-
-  Route::put('edit/{id}', ['as'=>'projeto.edit', 'uses'=>'ProjetoController@edit']);
-
-  Route::get('edit/{id}', ['as'=>'projeto.destroy', 'uses'=>'ProjetoController@destroy']);
-});
 
 
 
-Route::get('/home', 'HomeController@index');
+
+
+Route::get('/home', ['as' => 'home', 'uses' => 'HomeController@index']);
