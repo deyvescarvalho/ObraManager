8bec029c02cbe12a65af150e3b7e6f7ff6d94330
Deyves Carvalho <deyvescarvalho@gmail.com>
2017-03-22 16:59:47 -0300

    mudancas nos campos em fornecedor e adicionando relacionamentos

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -8,7 +8,23 @@ class Fornecedor extends Model
 {
     protected $fillable = [
       'descricao',
+      'ddd',
+      'telefone',
       'endereco',
-      'telefone'
+      'cidade'
     ];
+
+    public function projetos()
+    {
+      return $this->belongsToMany(Projeto::class, 'lancamento_projetos');
+    }
+
+    public function produtos()
+    {
+      return $this->belongsToMany(Produto::class, 'lancamento_projetos');
+    }
+
+
+
+
 }
