6c3e6989bc424c314b4f88595fa2b70ca623913a
Deyves Carvalho <deyvescarvalho@gmail.com>
2017-03-28 19:34:15 -0300

    correção para não mostrar tabela

--- resources/views/cliente/listagem.blade.php
+++ resources/views/cliente/listagem.blade.php
@@ -8,61 +8,66 @@
 
 @section('conteudo')
 
-@if (session('status'))
-<div class="alert success">
-  <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
-  {{ session('status') }}
-</div>
+  @if (session('status'))
+    <div class="alert success">
+      <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+      {{ session('status') }}
+    </div>
 
-@endif
+  @endif
 
 
-<div class="mdl-cell mdl-cell--12-col">
-  <a href="{{ route('cliente.novo') }}" class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab">
-    <i class="material-icons">add</i>
-  </a> Novo cliente
-  <table class="mdl-cell mdl-cell--12-col mdl-data-table mdl-js-data-table mdl-shadow--2dp ">
-    <thead>
-      <tr>
-        <th class="mdl-data-table__header--sorted-ascending mdl-data-table__cell--non-numeric">Nome</th>
-        <th class="mdl-data-table__cell--non-numeric">Idade</th>
-        <th class="mdl-data-table__cell--non-numeric">Data de nascimento</th>
-        <th class="mdl-data-table__cell--non-numeric">CPF</th>
-        <th class="mdl-data-table__cell--non-numeric">Telefone</th>
-        <th class="mdl-data-table__cell--non-numeric" colspan="2"></th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach($clientes as $cliente)
-      <tr>
-        <td class="mdl-data-table__cell--non-numeric ">{{$cliente->nome}}</td>
-        <td class="mdl-data-table__cell--non-numeric">{{$cliente->idade}}</td>
-        <td class="mdl-data-table__cell--non-numeric">{{$cliente->dia}}/{{$cliente->mes}}/{{$cliente->ano}}</td>
-        <td class="mdl-data-table__cell--non-numeric">{{$cliente->cpf}}</td>
-        <td class="mdl-data-table__cell--non-numeric">({{$cliente->ddd}}) - {{$cliente->telefone}}</td>
-        <td class="mdl-data-table__cell--non-numeric"><a href="{{ route('cliente.edit', ['id'=>$cliente->id]) }}" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored"><i class="material-icons">edit</i></a></td>
-        <td class="mdl-data-table__cell--non-numeric"><a href="{{ route('cliente.destroy', ['id'=>$cliente->id]) }}" onClick="return confirm('Deseja realmente deletar o cliente ?')" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored"><i class="material-icons">delete</i></a></td>
-      </tr>
-      @endforeach
-    </tbody>
-  </table>
-  <style media="screen">
-  ul.pagination {
-    display: inline-block;
-    padding: 0;
-    margin: 0;
-}
+  <div class="mdl-cell mdl-cell--12-col">
+    <a href="{{ route('cliente.novo') }}" class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab">
+      <i class="material-icons">add</i>
+    </a> Novo cliente
+    @if (count($clientes) > 0)
 
-ul.pagination li {display: inline;}
+      <table class="mdl-cell mdl-cell--12-col mdl-data-table mdl-js-data-table mdl-shadow--2dp ">
+        <thead>
+          <tr>
+            <th class="mdl-data-table__header--sorted-ascending mdl-data-table__cell--non-numeric">Nome</th>
+            <th class="mdl-data-table__cell--non-numeric">Idade</th>
+            <th class="mdl-data-table__cell--non-numeric">Data de nascimento</th>
+            <th class="mdl-data-table__cell--non-numeric">CPF</th>
+            <th class="mdl-data-table__cell--non-numeric">Telefone</th>
+            <th class="mdl-data-table__cell--non-numeric" colspan="2"></th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($clientes as $cliente)
+            <tr>
+              <td class="mdl-data-table__cell--non-numeric ">{{$cliente->nome}}</td>
+              <td class="mdl-data-table__cell--non-numeric">{{$cliente->idade}}</td>
+              <td class="mdl-data-table__cell--non-numeric">{{$cliente->dia}}/{{$cliente->mes}}/{{$cliente->ano}}</td>
+              <td class="mdl-data-table__cell--non-numeric">{{$cliente->cpf}}</td>
+              <td class="mdl-data-table__cell--non-numeric">({{$cliente->ddd}}) - {{$cliente->telefone}}</td>
+              <td class="mdl-data-table__cell--non-numeric"><a href="{{ route('cliente.edit', ['id'=>$cliente->id]) }}" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored"><i class="material-icons">edit</i></a></td>
+              <td class="mdl-data-table__cell--non-numeric"><a href="{{ route('cliente.destroy', ['id'=>$cliente->id]) }}" onClick="return confirm('Deseja realmente deletar o cliente ?')" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored"><i class="material-icons">delete</i></a></td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+      {{ $clientes->render()}}
+    @else
+      <h2>Não hà clientes cadastrados</h2>
+    @endif
+    <style media="screen">
+    ul.pagination {
+      display: inline-block;
+      padding: 0;
+      margin: 0;
+    }
 
-ul.pagination li a {
-    color: black;
-    float: left;
-    padding: 8px 16px;
-    text-decoration: none;
-}
+    ul.pagination li {display: inline;}
 
-  </style>
-  {{ $clientes->render()}}
-</div>
+    ul.pagination li a {
+      color: black;
+      float: left;
+      padding: 8px 16px;
+      text-decoration: none;
+    }
+
+    </style>
+  </div>
 @endsection
