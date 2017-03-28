aa349a8f78e651705b207126f7a7c5482c539ceb
Deyves Carvalho <deyvescarvalho@gmail.com>
2017-03-28 19:37:45 -0300

    modificação na view do funcionario

--- resources/views/funcionario/listagem.blade.php
+++ resources/views/funcionario/listagem.blade.php
@@ -8,69 +8,75 @@
 
 @section('conteudo')
 
-@if (session('status'))
-<div class="alert success">
-  <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
-  {{ session('status') }}
-</div>
+  @if (session('status'))
+    <div class="alert success">
+      <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+      {{ session('status') }}
+    </div>
 
-@endif
+  @endif
 
 
-<div class="mdl-cell mdl-cell--12-col">
-  <a href="{{ route('funcionario.create') }}" class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab">
-    <i class="material-icons">add</i>
-  </a> Novo funcionário
-  <table class="mdl-cell mdl-cell--12-col mdl-data-table mdl-js-data-table mdl-shadow--2dp ">
-    <thead>
-      <tr>
-        <th class="mdl-data-table__header--sorted-ascending mdl-data-table__cell--non-numeric">Nome</th>
-        <th class="mdl-data-table__cell--non-numeric">Idade</th>
-        <th class="mdl-data-table__cell--non-numeric">Data de nascimento</th>
-        <th class="mdl-data-table__cell--non-numeric">CPF</th>
-        <th class="mdl-data-table__cell--non-numeric">Cargo</th>
-        <th class="mdl-data-table__cell--non-numeric">Telefone</th>
-        <th class="mdl-data-table__cell--non-numeric" colspan="2"></th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach($funcionarios as $funcionario)
-      <tr>
-        <td class="mdl-data-table__cell--non-numeric ">{{$funcionario->nome}}</td>
-        <td class="mdl-data-table__cell--non-numeric">{{$funcionario->idade}}</td>
-        <td class="mdl-data-table__cell--non-numeric">{{$funcionario->dia}}/{{$funcionario->mes}}/{{$funcionario->ano}}</td>
-        <td class="mdl-data-table__cell--non-numeric">{{$funcionario->cpf}}</td>
-        @if ($funcionario->cargo)
+  <div class="mdl-cell mdl-cell--12-col">
+    <a href="{{ route('funcionario.create') }}" class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab">
+      <i class="material-icons">add</i>
+    </a> Novo funcionário
 
-          <td class="mdl-data-table__cell--non-numeric">{{$funcionario->cargo->descricao}}</td>
-        @else
-          <td class="mdl-data-table__cell--non-numeric"> -- Sem profissão -- </td>
+    @if (count($funcionarios) > 0)
 
-        @endif
-        <td class="mdl-data-table__cell--non-numeric">({{$funcionario->ddd}}) - {{$funcionario->telefone}}</td>
-        <td class="mdl-data-table__cell--non-numeric"><a href="{{ route('funcionario.edit', ['id'=>$funcionario->id]) }}" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored"><i class="material-icons">edit</i></a></td>
-        <td class="mdl-data-table__cell--non-numeric"><a href="{{ route('funcionario.destroy', ['id'=>$funcionario->id]) }}" onClick="return confirm('Deseja realmente deletar o funcionario ?')" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored"><i class="material-icons">delete</i></a></td>
-      </tr>
-      @endforeach
-    </tbody>
-  </table>
-  <style media="screen">
-  ul.pagination {
-    display: inline-block;
-    padding: 0;
-    margin: 0;
-}
+    <table class="mdl-cell mdl-cell--12-col mdl-data-table mdl-js-data-table mdl-shadow--2dp ">
+      <thead>
+        <tr>
+          <th class="mdl-data-table__header--sorted-ascending mdl-data-table__cell--non-numeric">Nome</th>
+          <th class="mdl-data-table__cell--non-numeric">Idade</th>
+          <th class="mdl-data-table__cell--non-numeric">Data de nascimento</th>
+          <th class="mdl-data-table__cell--non-numeric">CPF</th>
+          <th class="mdl-data-table__cell--non-numeric">Cargo</th>
+          <th class="mdl-data-table__cell--non-numeric">Telefone</th>
+          <th class="mdl-data-table__cell--non-numeric" colspan="2"></th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($funcionarios as $funcionario)
+          <tr>
+            <td class="mdl-data-table__cell--non-numeric ">{{$funcionario->nome}}</td>
+            <td class="mdl-data-table__cell--non-numeric">{{$funcionario->idade}}</td>
+            <td class="mdl-data-table__cell--non-numeric">{{$funcionario->dia}}/{{$funcionario->mes}}/{{$funcionario->ano}}</td>
+            <td class="mdl-data-table__cell--non-numeric">{{$funcionario->cpf}}</td>
+            @if ($funcionario->cargo)
 
-ul.pagination li {display: inline;}
+              <td class="mdl-data-table__cell--non-numeric">{{$funcionario->cargo->descricao}}</td>
+            @else
+              <td class="mdl-data-table__cell--non-numeric"> -- Sem profissão -- </td>
 
-ul.pagination li a {
-    color: black;
-    float: left;
-    padding: 8px 16px;
-    text-decoration: none;
-}
+            @endif
+            <td class="mdl-data-table__cell--non-numeric">({{$funcionario->ddd}}) - {{$funcionario->telefone}}</td>
+            <td class="mdl-data-table__cell--non-numeric"><a href="{{ route('funcionario.edit', ['id'=>$funcionario->id]) }}" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored"><i class="material-icons">edit</i></a></td>
+            <td class="mdl-data-table__cell--non-numeric"><a href="{{ route('funcionario.destroy', ['id'=>$funcionario->id]) }}" onClick="return confirm('Deseja realmente deletar o funcionario ?')" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored"><i class="material-icons">delete</i></a></td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+    {{ $funcionarios->render()}}
+  @else
+    <h2>Não hà funcionários cadastrados</h2>
+  @endif
+    <style media="screen">
+    ul.pagination {
+      display: inline-block;
+      padding: 0;
+      margin: 0;
+    }
 
-  </style>
-  {{ $funcionarios->render()}}
-</div>
+    ul.pagination li {display: inline;}
+
+    ul.pagination li a {
+      color: black;
+      float: left;
+      padding: 8px 16px;
+      text-decoration: none;
+    }
+
+    </style>
+  </div>
 @endsection
