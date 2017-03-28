2d6b3f2ab17267f5cc5d83d6b5c30983b72ac06c
Deyves Carvalho <deyvescarvalho@gmail.com>
2017-03-28 19:43:07 -0300

    add projetos if

--- resources/views/projeto/listagem.blade.php
+++ resources/views/projeto/listagem.blade.php
@@ -21,58 +21,65 @@
     <a href="{{ route('projeto.create') }}" class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab">
       <i class="material-icons">add</i>
     </a> Novo projeto
-    <table class="mdl-cell mdl-cell--12-col mdl-data-table mdl-js-data-table mdl-shadow--2dp ">
-      <thead>
-        <tr>
-          <th class="mdl-data-table__header--sorted-ascending mdl-data-table__cell--non-numeric">Cliente</th>
-          <th class="mdl-data-table__cell--non-numeric">Valor estimado R$</th>
-          <th class="mdl-data-table__cell--non-numeric">Endereço</th>
-          <th class="mdl-data-table__cell--non-numeric" colspan="4"></th>
-        </tr>
-      </thead>
-      <tbody>
-        @php
-        $count =1
-        @endphp
-        @foreach($projetos as $projeto)
+    @if (count($projetos) > 0)
+
+
+      <table class="mdl-cell mdl-cell--12-col mdl-data-table mdl-js-data-table mdl-shadow--2dp ">
+        <thead>
           <tr>
-
-            <td class="mdl-data-table__cell--non-numeric ">{{$projeto->cliente->nome}}</td>
-            <td class="mdl-data-table__cell--non-numeric">{{ number_format($projeto->valorobra, 2, ',', '.')}}</td>
-            <td class="mdl-data-table__cell--non-numeric">{{$projeto->endereco}} - {{ $projeto->cidade }}</td>
-            <td id="viewProject{{print_r($count)}}" class="mdl-data-table__cell--non-numeric">
-              <a href="{{ route('projeto.view', ['id'=>$projeto->id]) }}" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored">
-                <i class="material-icons">visibility</i>
-              </a>
-              <div class="mdl-tooltip" for="viewProject{{print_r($count)}}">Visualizar detalhes do projeto</div>
-            </td>
-
-            <td id="addLancamento{{print_r($count)}}" class="mdl-data-table__cell--non-numeric">
-              <a href="{{ route('lancamento.create', ['id'=>$projeto->id]) }}" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored">
-                <i class="material-icons">add</i>
-              </a>
-              <div class="mdl-tooltip" for="addLancamento{{print_r($count)}}">Novo lançamento no projeto</div>
-            </td>
-
-            @php
-            $count++
-            @endphp
-
-            <td class="mdl-data-table__cell--non-numeric">
-              <a href="{{ route('projeto.edit', ['id'=>$projeto->id]) }}" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored">
-                <i class="material-icons">edit</i>
-              </a>
-            </td>
-
-            <td class="mdl-data-table__cell--non-numeric">
-              <a href="{{ route('projeto.destroy', ['id'=>$projeto->id]) }}" onClick="return confirm('Deseja realmente deletar o projeto ?')" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored">
-                <i class="material-icons">delete</i>
-              </a>
-            </td>
+            <th class="mdl-data-table__header--sorted-ascending mdl-data-table__cell--non-numeric">Cliente</th>
+            <th class="mdl-data-table__cell--non-numeric">Valor estimado R$</th>
+            <th class="mdl-data-table__cell--non-numeric">Endereço</th>
+            <th class="mdl-data-table__cell--non-numeric" colspan="4"></th>
           </tr>
-        @endforeach
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          @php
+          $count =1
+          @endphp
+          @foreach($projetos as $projeto)
+            <tr>
+
+              <td class="mdl-data-table__cell--non-numeric ">{{$projeto->cliente->nome}}</td>
+              <td class="mdl-data-table__cell--non-numeric">{{ number_format($projeto->valorobra, 2, ',', '.')}}</td>
+              <td class="mdl-data-table__cell--non-numeric">{{$projeto->endereco}} - {{ $projeto->cidade }}</td>
+              <td id="viewProject{{print_r($count)}}" class="mdl-data-table__cell--non-numeric">
+                <a href="{{ route('projeto.view', ['id'=>$projeto->id]) }}" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored">
+                  <i class="material-icons">visibility</i>
+                </a>
+                <div class="mdl-tooltip" for="viewProject{{print_r($count)}}">Visualizar detalhes do projeto</div>
+              </td>
+
+              <td id="addLancamento{{print_r($count)}}" class="mdl-data-table__cell--non-numeric">
+                <a href="{{ route('lancamento.create', ['id'=>$projeto->id]) }}" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored">
+                  <i class="material-icons">add</i>
+                </a>
+                <div class="mdl-tooltip" for="addLancamento{{print_r($count)}}">Novo lançamento no projeto</div>
+              </td>
+
+              @php
+              $count++
+              @endphp
+
+              <td class="mdl-data-table__cell--non-numeric">
+                <a href="{{ route('projeto.edit', ['id'=>$projeto->id]) }}" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored">
+                  <i class="material-icons">edit</i>
+                </a>
+              </td>
+
+              <td class="mdl-data-table__cell--non-numeric">
+                <a href="{{ route('projeto.destroy', ['id'=>$projeto->id]) }}" onClick="return confirm('Deseja realmente deletar o projeto ?')" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored">
+                  <i class="material-icons">delete</i>
+                </a>
+              </td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+      {{ $projetos->render() }}
+    @else
+      <h2>Não hà projetos cadastrados</h2>
+    @endif
     <style media="screen">
     ul.pagination {
       display: inline-block;
@@ -90,6 +97,5 @@
     }
 
     </style>
-    {{ $projetos->render() }}
   </div>
 @endsection
