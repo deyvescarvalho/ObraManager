bfdaf71497e168ba262b73b1075a006e3094f2b0
Deyves Carvalho <deyvescarvalho@gmail.com>
2017-03-28 19:46:08 -0300

    simplificando cargo

--- resources/views/funcionario/listagem.blade.php
+++ resources/views/funcionario/listagem.blade.php
@@ -24,43 +24,37 @@
 
     @if (count($funcionarios) > 0)
 
-    <table class="mdl-cell mdl-cell--12-col mdl-data-table mdl-js-data-table mdl-shadow--2dp ">
-      <thead>
-        <tr>
-          <th class="mdl-data-table__header--sorted-ascending mdl-data-table__cell--non-numeric">Nome</th>
-          <th class="mdl-data-table__cell--non-numeric">Idade</th>
-          <th class="mdl-data-table__cell--non-numeric">Data de nascimento</th>
-          <th class="mdl-data-table__cell--non-numeric">CPF</th>
-          <th class="mdl-data-table__cell--non-numeric">Cargo</th>
-          <th class="mdl-data-table__cell--non-numeric">Telefone</th>
-          <th class="mdl-data-table__cell--non-numeric" colspan="2"></th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach($funcionarios as $funcionario)
+      <table class="mdl-cell mdl-cell--12-col mdl-data-table mdl-js-data-table mdl-shadow--2dp ">
+        <thead>
           <tr>
-            <td class="mdl-data-table__cell--non-numeric ">{{$funcionario->nome}}</td>
-            <td class="mdl-data-table__cell--non-numeric">{{$funcionario->idade}}</td>
-            <td class="mdl-data-table__cell--non-numeric">{{$funcionario->dia}}/{{$funcionario->mes}}/{{$funcionario->ano}}</td>
-            <td class="mdl-data-table__cell--non-numeric">{{$funcionario->cpf}}</td>
-            @if ($funcionario->cargo)
-
-              <td class="mdl-data-table__cell--non-numeric">{{$funcionario->cargo->descricao}}</td>
-            @else
-              <td class="mdl-data-table__cell--non-numeric"> -- Sem profissão -- </td>
-
-            @endif
-            <td class="mdl-data-table__cell--non-numeric">({{$funcionario->ddd}}) - {{$funcionario->telefone}}</td>
-            <td class="mdl-data-table__cell--non-numeric"><a href="{{ route('funcionario.edit', ['id'=>$funcionario->id]) }}" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored"><i class="material-icons">edit</i></a></td>
-            <td class="mdl-data-table__cell--non-numeric"><a href="{{ route('funcionario.destroy', ['id'=>$funcionario->id]) }}" onClick="return confirm('Deseja realmente deletar o funcionario ?')" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored"><i class="material-icons">delete</i></a></td>
+            <th class="mdl-data-table__header--sorted-ascending mdl-data-table__cell--non-numeric">Nome</th>
+            <th class="mdl-data-table__cell--non-numeric">Idade</th>
+            <th class="mdl-data-table__cell--non-numeric">Data de nascimento</th>
+            <th class="mdl-data-table__cell--non-numeric">CPF</th>
+            <th class="mdl-data-table__cell--non-numeric">Cargo</th>
+            <th class="mdl-data-table__cell--non-numeric">Telefone</th>
+            <th class="mdl-data-table__cell--non-numeric" colspan="2"></th>
           </tr>
-        @endforeach
-      </tbody>
-    </table>
-    {{ $funcionarios->render()}}
-  @else
-    <h2>Não hà funcionários cadastrados</h2>
-  @endif
+        </thead>
+        <tbody>
+          @foreach($funcionarios as $funcionario)
+            <tr>
+              <td class="mdl-data-table__cell--non-numeric ">{{$funcionario->nome}}</td>
+              <td class="mdl-data-table__cell--non-numeric">{{$funcionario->idade}}</td>
+              <td class="mdl-data-table__cell--non-numeric">{{$funcionario->dia}}/{{$funcionario->mes}}/{{$funcionario->ano}}</td>
+              <td class="mdl-data-table__cell--non-numeric">{{$funcionario->cpf}}</td>
+              <td class="mdl-data-table__cell--non-numeric">{{$funcionario->cargo->descricao or 'Sem profissão'}}</td>
+              <td class="mdl-data-table__cell--non-numeric">({{$funcionario->ddd}}) - {{$funcionario->telefone}}</td>
+              <td class="mdl-data-table__cell--non-numeric"><a href="{{ route('funcionario.edit', ['id'=>$funcionario->id]) }}" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored"><i class="material-icons">edit</i></a></td>
+              <td class="mdl-data-table__cell--non-numeric"><a href="{{ route('funcionario.destroy', ['id'=>$funcionario->id]) }}" onClick="return confirm('Deseja realmente deletar o funcionario ?')" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored"><i class="material-icons">delete</i></a></td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+      {{ $funcionarios->render()}}
+    @else
+      <h2>Não hà funcionários cadastrados</h2>
+    @endif
     <style media="screen">
     ul.pagination {
       display: inline-block;
