84322e54453641b8b76e4467f019b88140a5ecdb
Deyves Carvalho <deyvescarvalho@gmail.com>
2017-04-07 20:31:55 -0300

    mudancas no request

---
+++ app/Http/Requests/FuncionarioRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class FuncionarioRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+          'nome' => 'required',
+          'idade' => 'required|min:2|size:2',
+          'email' => 'email',
+          'cidade' => 'required',
+          'cargo_id' => 'required'
+        ];
+    }
+
+    public function messages()
+    {
+      return [
+        'nome.required' => 'Atenção! Informe um nome!',
+        'idade.required' => 'Atenção! Informe uma idade!',
+        'idade.min' => 'Atenção! A quantidade mínima de caracteres é 2.',
+        'idade.size' => 'Atenção! A quantidade mínima de caracteres é 2.',
+        'email.email' => 'Atenção! Informe um e-mail válido!',
+        'cidade.required' => 'Atenção! Informe uma cidade!',
+        'cargo_id.required' => 'Atenção! Informe um cargo!'
+      ];
+    }
+}
