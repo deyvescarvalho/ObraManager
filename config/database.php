1474234efd413869c183693eca1ec772f0fba29e
Deyves Carvalho <deyvescarvalho@gmail.com>
2017-05-15 10:06:49 -0300

    teste

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'psql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
