42f1356a1513e76478c475da00b9b56ed7b816dc
Deyves Carvalho <deyvescarvalho@gmail.com>
2017-05-23 21:13:40 -0300

    inclusao de grafico

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -110,7 +110,34 @@
     <!-- /.info-box -->
   </div>
 
+    <div class="mdl-cell mdl-cell--6-col-desktop">
+      <div id="piechart" style="height:400px;"></div>
+    </div>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+      <script type="text/javascript">
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
 
+        function drawChart() {
+
+          var data = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
+            ['Work',     11],
+            ['Eat',      2],
+            ['Commute',  2],
+            ['Watch TV', 2],
+            ['Sleep',    7]
+          ]);
+
+          var options = {
+            title: 'Projetos por clientes'
+          };
+
+          var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+          chart.draw(data, options);
+        }
+      </script>
 
 
 
