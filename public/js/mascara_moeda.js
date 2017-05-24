6f1d884e38530ce333d810ea237bd9f27ddf5232
Deyves Carvalho <deyvescarvalho@gmail.com>
2017-05-24 18:44:22 -0300

    modificacao no js

--- public/js/mascara_moeda.js
+++ public/js/mascara_moeda.js
@@ -91,12 +91,12 @@ function data(v){
   return v
 }
 
-function moeda(v){
-  v=v.replace(/\D/g,"") // permite digitar apenas numero
-  v=v.replace(/(\d{1})(\d{17})$/,"$1.$2") // coloca ponto antes dos ultimos digitos
-  v=v.replace(/(\d{1})(\d{13})$/,"$1.$2") // coloca ponto antes dos ultimos 13 digitos
-  v=v.replace(/(\d{1})(\d{10})$/,"$1.$2") // coloca ponto antes dos ultimos 10 digitos
-  v=v.replace(/(\d{1})(\d{7})$/,"$1.$2") // coloca ponto antes dos ultimos 7 digitos
-  v=v.replace(/(\d{1})(\d{1,4})$/,"$1,$2") // coloca virgula antes dos ultimos 4 digitos
-  return v;
-}
+// function moeda(v){
+//   v=v.replace(/\D/g,"") // permite digitar apenas numero
+//   v=v.replace(/(\d{1})(\d{17})$/,"$1.$2") // coloca ponto antes dos ultimos digitos
+//   v=v.replace(/(\d{1})(\d{13})$/,"$1.$2") // coloca ponto antes dos ultimos 13 digitos
+//   v=v.replace(/(\d{1})(\d{9})$/,"$1.$2") // coloca ponto antes dos ultimos 10 digitos
+//   v=v.replace(/(\d{1})(\d{5})$/,"$1.$2") // coloca ponto antes dos ultimos 7 digitos
+//   v=v.replace(/(\d{1})(\d{1,2})$/,"$1,$2") // coloca virgula antes dos ultimos 4 digitos
+//   return v;
+// }
