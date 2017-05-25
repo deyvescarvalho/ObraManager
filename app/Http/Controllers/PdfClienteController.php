af23b37d227eb9165914ad13b4bbfe9984bd379d
Deyves Carvalho <deyvescarvalho@gmail.com>
2017-05-25 16:26:10 -0300

    arquivos para funcionar relatorio

--- app/Http/Controllers/PdfClienteController.php
+++ app/Http/Controllers/PdfClienteController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Cliente;
-use Barryvdh\Snappy\Facades\SnappyPdf as PDF;
+// use Barryvdh\Snappy\Facades\SnappyPdf as PDF;
+use Barryvdh\DomPDF\Facade as PDF;
 use Auth;
 class PdfClienteController extends Controller
 {
@@ -14,7 +15,7 @@ class PdfClienteController extends Controller
     {
       $clientes = Cliente::where('user_id', Auth::getUser()->id)->get();
       $clientesAniversario = Cliente::where('user_id', Auth::getUser()->id)->where('dtNascimento', '2222-02-25')->get();
-      $pdf = PDF::loadView('cliente.pdf', ['clientes' => $clientesAniversario]);
-      return $pdf->stream();
+      $pdf = PDF::loadView('cliente.pdf', ['clientes' => $clientes]);
+      return $pdf->stream('cliente.pdf');
     }
 }
